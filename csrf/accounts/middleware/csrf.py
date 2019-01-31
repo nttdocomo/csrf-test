12f12cf360595853cbd8ff780c35865fbf188895
ouyi <ouyi@didichuxing.com>
2019-01-31 15:38:59 +0800

    update

--- csrf/accounts/middleware/csrf.py
+++ csrf/accounts/middleware/csrf.py
@@ -42,6 +42,17 @@ def _sanitize_token(token):
         return _get_new_csrf_key()
     return token
 
+
+
+class DisableCSRFMiddleware(object):
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        setattr(request, '_dont_enforce_csrf_checks', True)
+        response = self.get_response(request)
+        return response
+
 class CsrfViewMiddleware(object):
     """
     Middleware that requires a present and correct csrfmiddlewaretoken
