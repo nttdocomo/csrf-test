b2b3a19c3e374c8430836eac2d8645c2be4dce0d
ouyi <ouyi@didichuxing.com>
2019-01-31 16:03:12 +0800

    update

--- csrf/accounts/middleware/csrf.py
+++ csrf/accounts/middleware/csrf.py
@@ -45,13 +45,8 @@ def _sanitize_token(token):
 
 
 class DisableCSRFMiddleware(object):
-    def __init__(self, get_response):
-        self.get_response = get_response
-
-    def __call__(self, request):
+    def process_request(self, request):
         setattr(request, '_dont_enforce_csrf_checks', True)
-        response = self.get_response(request)
-        return response
 
 class CsrfViewMiddleware(object):
     """
